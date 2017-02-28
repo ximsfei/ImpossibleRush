bd9288990543e33fb34124dbe7aa94abd035d083
pengfengwang <550616352@qq.com>
2017-02-28 16:25:38 +0800

    SplashActivity
    Signed-off-by: pengfengwang <550616352@qq.com>

--- app/src/main/java/com/ximsfei/rush/util/RushConstants.java
+++ app/src/main/java/com/ximsfei/rush/util/RushConstants.java
@@ -5,9 +5,14 @@ package com.ximsfei.rush.util;
  */
 
 public class RushConstants {
-    public static final long HIGH_LEVEL_DURATION = 300;
-    public static final long MEDIUM_LEVEL_DURATION = 600;
-    public static final long LOW_LEVEL_DURATION = 900;
+    public static final String APPID = "1105933657";
+    public static final String BannerPosID = "9079537218417626401";
+    public static final String APPWallPosID = "9007479624379698465";
+    public static final String InterteristalPosID = "8575134060152130849";
+    public static final String SplashPosID = "8863364436303842593";
+    public static final String GridAppWallPosID = "9007479624379698465";
+    public static final String NativePosID = "5000709048439488";
+    public static final String NativeVideoPosID = "2050206699818455";
 
     public static final String KEY_GAME_MODE_DEFAULT = "game-mode"; // 4 color, 6 color
 }
